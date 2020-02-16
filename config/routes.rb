da1a4afd2e6fd61e5712ff623db463f27e40dc51
Melissa Saw <melissasaw95@hotmail.com>
2020-02-16 23:43:55 +0800

    Completed CRUD for user, user able to update first, last and email, and also delete user account

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
 
   root to: "users#index"
 
-
-
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
+  resources :users, only: [:update, :show, :edit, :index, :destroy]
   resources :users, controller: "users", only: [:create] do
     resource :password,
       controller: "clearance/passwords",
@@ -17,4 +16,6 @@ Rails.application.routes.draw do
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 end
